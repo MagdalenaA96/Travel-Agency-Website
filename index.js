12ac064fe0e42e3d52f50b4bf504e265bae3755f
MagdalenaA96 <kowalikmagdalena1996@gmail.com>
2024-09-16 15:05:15 +0200

    Adding dynamic year to offers (offers.js), adding EventListener to offerDiv, creating function ShowOfferDetails, updating styles, adding new section in index.html

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const $offersList = document.getElementById("offers-list");
+const $choosenOffer = document.getElementById("choosen-offer");
+const $introduceSection = document.getElementById("introduce-section");
 
 function showOffers() {
   let offers = window.travelOffers;
@@ -11,23 +13,46 @@ function showOffers() {
     offerDiv.classList.add("offer-box");
     let imageDiv = document.createElement("div");
     let descriptionPlace = document.createElement("div");
+    descriptionPlace.classList.add("decription-style");
 
     offerDiv.appendChild(imageDiv);
     offerDiv.appendChild(descriptionPlace);
 
     let offerImage = document.createElement("img");
-    offerImage.src = "static/images/chor-tsang-07mSKrzKiRw-unsplash.jpg"
+    offerImage.classList.add("offer-image");
+    offerImage.src = "static/images/chor-tsang-07mSKrzKiRw-unsplash.jpg";
     offerImage.alt = "Offer's picture";
     imageDiv.appendChild(offerImage);
 
     let description = document.createElement("p");
-    description.innerText = `${offer.hotel}
-    ${offer.country}
-    ${offer.city}
-    ${offer.dates[0].departure} - ${offer.dates[0].return}
-    ${offer.price} zł`;
+    description.innerHTML = `<h3>${offer.hotel}</h3>
+    <h4>${offer.country}, ${offer.city}</h4>
+    <p>${offer.dates[0].departure} - ${offer.dates[0].return}</p>
+    <p class="price-style">${offer.price} zł <span>/os</span></p>`;
     descriptionPlace.appendChild(description);
 
     $offersList.appendChild(offerDiv);
+
+    offerDiv.addEventListener("click", () => {
+      showOfferDetails(offer);
+    });
+
+    return { offer: offer, offerDiv: offerDiv };
   });
+}
+
+function showOfferDetails(offer) {
+  $choosenOffer.classList.toggle("hidden");
+  $offersList.classList.toggle("hidden");
+  $introduceSection.classList.toggle("hidden");
+
+  $hotelName = document.getElementById("hotel-name");
+  $hotelName.innerText = `${offer.hotel}`;
+
+  $myOfferDetails = document.getElementById("travel-details-container");
+  $myOfferDetails.innerHTML = ``;
+  $myOfferDetails.innerHTML = `<h3>${offer.hotel}</h3>
+    <h4>${offer.country}, ${offer.city}</h4>
+    <p>${offer.dates[0].departure} - ${offer.dates[0].return}</p>
+    <p class="price-style">${offer.price} zł <span>/os</span></p>`;
 }
