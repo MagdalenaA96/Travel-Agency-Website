8c7adf75fb7cd6f1351e67a36ad83ec6c4820fca
MagdalenaA96 <kowalikmagdalena1996@gmail.com>
2024-09-24 13:17:39 +0200

    Creating HTML sections and functions to display appropriate offers. Creating more travel offers. Updating navigation to hide/show sections and manage history. Refactoring the rest of the code (e.g., back buttons) to accommodate these changes.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const initialHash = window.location.hash || "#home-page"; // Sprawdza, czy URL zawiera hash, jeśli nie, ustawia #home-page
+  showSection(initialHash);
+  window.history.replaceState({}, "", initialHash);
+
   showOffers();
+  showSummerOffers();
+  showWinterOffers();
 });
 
+const $homePage = document.getElementById("home-page");
 const $introduceSection = document.getElementById("introduce-section");
 const $searchInput = document.getElementById("search-input");
 const $searchButton = document.getElementById("search-button");
 const $offersList = document.getElementById("offers-list");
 const offers = window.travelOffers;
+const sections = document.querySelectorAll("main > section");
+const $foundOffersSection = document.getElementById("found-offers");
 const $choosenOffer = document.getElementById("choosen-offer");
 const $myOfferDescription = document.getElementById("my-offer-description");
 const $selectedBoard = document.getElementById("board");
@@ -23,9 +32,88 @@ let saveOfferHandler = null;
 const bookingDetails = {};
 let selectedOffer = null;
 
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetSection = link.getAttribute("href");
+
+    showSection(targetSection);
+
+    window.history.pushState({}, "", targetSection);
+  });
+});
+
+function showSection(sectionId) {
+  hideSection();
+
+  const targetSection = document.querySelector(sectionId);
+  if (targetSection) {
+    targetSection.classList.remove("hidden");
+  } else {
+    showSection("#home-page");
+  }
+}
+
+function hideSection() {
+  sections.forEach((section) => {
+    section.classList.add("hidden");
+  });
+}
+
+window.addEventListener("popstate", () => {
+  const hash = window.location.hash;
+
+  showSection(hash);
+});
+
 function showOffers() {
   offers.map((offer) => {
-    createOfferBox(offer);
+    const offerBox = createOfferBox(offer);
+    $offersList.appendChild(offerBox);
+
+    return { offer };
+  });
+}
+
+function showSummerOffers() {
+  let summerOffers = offers.filter((offer) =>
+    offer.dates.some(
+      (date) =>
+        date.departure.includes("-06-") ||
+        date.departure.includes("-07-") ||
+        date.departure.includes("-08-") ||
+        date.departure.includes("-09-")
+    )
+  );
+
+  summerOffers.map((offer) => {
+    const $summerOffersScreen = document.getElementById(
+      "summer-offers-container"
+    );
+    const offerBox = createOfferBox(offer);
+    $summerOffersScreen.appendChild(offerBox);
+
+    return { offer };
+  });
+}
+
+function showWinterOffers() {
+  let winterOffers = offers.filter((offer) =>
+    offer.dates.some(
+      (date) =>
+        date.departure.includes("-12-") ||
+        date.departure.includes("-01-") ||
+        date.departure.includes("-02-") ||
+        date.departure.includes("-03-")
+    )
+  );
+
+  winterOffers.map((offer) => {
+    const $winterOffersScreen = document.getElementById(
+      "winter-offers-container"
+    );
+    const offerBox = createOfferBox(offer);
+    $winterOffersScreen.appendChild(offerBox);
 
     return { offer };
   });
@@ -54,15 +142,13 @@ function createOfferBox(offer) {
     <p class="price-style">${offer.price} zł <span>/os</span></p>`;
   descriptionPlace.appendChild(description);
 
-  $offersList.appendChild(offerDiv);
-
   offerDiv.addEventListener("mouseenter", function () {
     this.style.cursor = "pointer";
-    offerDiv.classList.add("actual-offer")
+    offerDiv.classList.add("actual-offer");
   });
 
   offerDiv.addEventListener("mouseleave", function () {
-    offerDiv.classList.remove("actual-offer")
+    offerDiv.classList.remove("actual-offer");
   });
 
   offerDiv.addEventListener("click", () => {
@@ -70,6 +156,8 @@ function createOfferBox(offer) {
     console.log(selectedOffer);
     showOfferDetails(selectedOffer);
   });
+
+  return offerDiv;
 }
 
 $searchButton.addEventListener("mouseenter", function () {
@@ -79,21 +167,15 @@ $searchButton.addEventListener("mouseenter", function () {
 $searchButton.addEventListener("click", () => {
   const searchTerm = $searchInput.value.toLowerCase();
   filterOffers(searchTerm);
+  window.location.hash = "found-offers";
 });
 
 function filterOffers(searchTerm) {
-  const screens = [$choosenOffer, $bookingForm, $confirmationForm];
-  screens.forEach((screen) => {
-    if (!screen.classList.contains("hidden")) {
-      screen.classList.add("hidden");
-    }
-  });
+  hideSection();
 
-  if ($offersList.classList.contains("hidden")) {
-    $offersList.classList.remove("hidden");
-  }
-
-  $offersList.innerHTML = ``;
+  $foundOffersSection.classList.remove("hidden");
+  const $foundOffersList = document.getElementById("found-offers-list");
+  $foundOffersList.innerHTML = ``;
 
   let foundOffers = offers.filter(
     (offer) =>
@@ -101,18 +183,18 @@ function filterOffers(searchTerm) {
       offer.city.toLowerCase().includes(searchTerm)
   );
   if (foundOffers.length === 0) {
-    $offersList.innerHTML = "<p>No offers found</p>";
+    $foundOffersList.innerHTML = "<p>No offers found</p>";
   } else {
     foundOffers.forEach((offer) => {
-      createOfferBox(offer);
+      const offerBox = createOfferBox(offer);
+      $foundOffersList.appendChild(offerBox);
     });
   }
 }
 
 function showOfferDetails(offer) {
-  $choosenOffer.classList.toggle("hidden");
-  $offersList.classList.toggle("hidden");
-  $introduceSection.classList.add("hidden");
+  hideSection();
+  $choosenOffer.classList.remove("hidden");
 
   const $hotelName = document.getElementById("hotel-name");
   $hotelName.innerText = `${offer.hotel}`;
@@ -190,8 +272,9 @@ function showOfferDetails(offer) {
 const $backToOffersButton = document.getElementById("back-button-offer");
 $backToOffersButton.addEventListener("click", () => {
   $choosenOffer.classList.toggle("hidden");
-  $offersList.classList.toggle("hidden");
-  $introduceSection.classList.toggle("hidden");
+
+  const hash = window.location.hash;
+  showSection(hash);
 });
 
 function showTotalPrice(offer) {
@@ -307,9 +390,8 @@ function showConfirmationForm(offer, bookingDetails) {
 
 const $backToHomePageBtn = document.getElementById("back-to-homepage");
 $backToHomePageBtn.addEventListener("click", () => {
-  $confirmationForm.classList.toggle("hidden");
-  $offersList.classList.toggle("hidden");
-  $introduceSection.classList.toggle("hidden");
+  hideSection();
+  showSection("#home-page");
 
   $reservationForm.reset();
   selectedOffer = null;
